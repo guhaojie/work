b3342e09e7a682f42dffedeacb20ce35d8e8b4bb
haojiegu <haojiegu@gmail.com>
2024-09-02 17:21:34 +0800

    init commit
    Signed-off-by: haojiegu <haojiegu@gmail.com>

--- Mathmatics/FFR.py
+++ Mathmatics/FFR.py
@@ -18,7 +18,7 @@ def basic_gen(result=None):
     """
     根据提供的数，来生成一个结果是该数的算式
     :param result: 结果
-    :return: 格式为[[x,op,y],r]的算式
+    :return: 格式为{x,op,y,r}的算式
     """
     _r = random.randint(BOUND_L, BOUND_U) if result is None else result
     _op = random.choice(['+', '-', '×', '÷']) if _r != 0 else random.choice(['+', '-', '×'])
@@ -29,7 +29,7 @@ def basic_gen(result=None):
               lambda r, x: x - r],
         '×': [lambda r: random.choice([i for i in range(2, r) if r % i == 0] or [1, r]),
               lambda r, x: random.randint(BOUND_L, BOUND_U) if x == 0 else int(r / x)],
-        '÷': [lambda r: random.choice([i for i in range(1, BOUND_U//r)] or [1]) * r,
+        '÷': [lambda r: random.choice([i for i in range(1, BOUND_U // r)] or [1]) * r,
               lambda r, x: int(x / r)]
     }
     while True:
@@ -40,7 +40,10 @@ def basic_gen(result=None):
                 [_x, _y]))) == 2:
             break
 
-    return [[_x, _op, _y], _r]
+    return {'x': _x,
+            'op': _op,
+            'y': _y,
+            'r': _r}
 
 
 def expand_gen(fn, n, x):
@@ -52,9 +55,9 @@ def expand_gen(fn, n, x):
     :return:拓展的结果
     """
     if n != 0:
-        i = random.choice([0, 2])
-        x[0][i] = fn(x[0][i])
-        expand_gen(fn, n - 1, x[0][i])
+        i = random.choice(['x', 'y'])
+        x[i] = fn(x[i])
+        expand_gen(fn, n - 1, x[i])
     return x
 
 
@@ -64,38 +67,38 @@ def gen_print(x, last_op=None, lr=None):
     :param x: 公式
     :param last_op: 记录符号堆栈
     :param lr: 是否为右子树（减加、除乘不能去括号）
-    :return: 无
+    :return: 去括号算式
     """
     _op_order = {'+': 0, '-': 0, '×': 1, '÷': 1}
-    x0 = x[0]
+    _r = ""
     _wrap = False if last_op is None else (
-            (_op_order[last_op] > _op_order[x0[1]]) or
-            (last_op == '-' and _op_order[x0[1]] == 0 and lr == 'r') or
+            (_op_order[last_op] > _op_order[x['op']]) or
+            (last_op == '-' and _op_order[x['op']] == 0 and lr == 'r') or
             (last_op == '÷' and lr == 'r'))
     if _wrap:
-        print("(", end='')
-    if type(x0[0]) is list:
-        gen_print(x0[0], x0[1], 'l')
+        _r += "("
+    if type(x['x']) is dict:
+        _r += gen_print(x['x'], x['op'], 'l')
     else:
-        print(f"{x0[0]}", end='')
-    print(f" {x0[1]} ", end='')
-    if type(x0[2]) is list:
-        gen_print(x0[2], x0[1], 'r')
+        _r += f"{x['x']}"
+    _r += f" {x['op']} "
+    if type(x['y']) is dict:
+        _r += gen_print(x['y'], x['op'], 'r')
     else:
-        print(f"{x0[2]}", end='')
+        _r += f"{x['y']}"
     if _wrap:
-        print(")", end='')
+        _r += ")"
     if last_op is None:
-        print(" = ? ", end='')
+        _r += " = ? "
+    return _r
 
 
-for _ in range(10):
+for _ in range(2):
     x_o = expand_gen(basic_gen,
                      2,
                      basic_gen(random.randint(BOUND_L, BOUND_U)))
-    x_r = x_o[1]
-    print(f"{_+1}>\t", end='')
-    gen_print(x_o)
+    x_r = x_o['r']
+    print(f"{_ + 1}>\t{gen_print(x_o)}", end='')
     ans = input()
     if ans == str(x_r):
         print("It's right!")
