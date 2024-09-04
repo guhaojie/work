f92707a94efd760b7527058947a6ddf4e25ba634
haojiegu <haojiegu@gmail.com>
2024-09-04 18:37:25 +0800

    优化算法
    Signed-off-by: haojiegu <haojiegu@gmail.com>

--- ORG/ORG.py
+++ ORG/ORG.py
@@ -1,33 +1,61 @@
-class ORGANIZATION:
-    def __init__(self, info):
-        self.name = ""
-        self.type = ""
-        self.CEO = ""
-        self.parent = None
-        self.children = []
-        self.__dict__.update(info)
-
-    def __str__(self):
-        x = "\\_" if self.parent else ""
-        x += f"{self.name}"
-        for _ in self.children:
-            temp = f"\n{_}".replace('\n', '\n\t')
-            x += f"{temp}"
-        return x
+import uuid
 
 
-a = ORGANIZATION({'name': '支部一'})
-b = ORGANIZATION({'name': '总支二'})
-c = ORGANIZATION({'name': '党委一'})
-d = ORGANIZATION({'name': '党委二'})
-e = ORGANIZATION({'name': '党委三'})
+def create_node(parent=None) -> dict:
+    node = {'id': uuid.uuid1(),
+            'parent': parent,
+            'children': []}
+    return node
 
-a.parent = b
-b.children.append(a)
-b.parent = c
-c.children.append(b)
-c.parent = e
-e.children.append(c)
-d.parent = e
-e.children.append(d)
-print(e)
+
+def update_info(node: dict, info: dict) -> dict:
+    node.update(info)
+    return node
+
+
+def add_child(parent: dict, info=None) -> dict:
+    child = update_info(create_node(parent), info if info is not None else {})
+    parent['children'].append(child)
+    return child
+
+def find_node(find_from: dict, key_words: str, by: str = 'name') -> list:
+    match = []
+    if find_from.get(by, "") == key_words:
+        match.append(find_from)
+    else:
+        for _ in find_from['children']:
+            match += find_node(_, key_words, by)
+    return match
+
+
+def print_tree(r: dict, k=None, indent: int = 0) -> None:
+    k = ['id'] if k is None else k
+    if r == {}:
+        print(f'There is nothing in {r}.')
+    else:
+        print("\t" * indent, end="")
+        print("\\_", end="")
+        for _ in k:
+            print(r.get(_, f"未发现 {_} 相关信息"), "\t", end="")
+        print("\n", end="")
+        for n in r['children']:
+            print_tree(n, k, indent + 1)
+
+
+ROOT = update_info(create_node(), info={'name': 'root'})
+add_child(
+    add_child(ROOT,
+              info={'name': 'c1'}),
+    info={'name': 'c2'})
+add_child(
+    add_child(
+        add_child(
+            add_child(ROOT,
+                      info={'name': 'c3'}),
+            info={'name': 'c4'}),
+        info={'name': 'c5'}),
+    info={'name': 'c6'})
+add_child(ROOT, info={'name': 'c7'})
+print_tree(ROOT, k=['name'])
+
+print_tree(find_node(ROOT, 'c1')[0], k=['id', 'name'])
