e2ae0e2f865c4a9d745171436b44d813bd18e5d1
haojiegu <haojiegu@gmail.com>
2024-09-14 10:46:25 +0800

    优化算法 - 优化了可读性
    Signed-off-by: haojiegu <haojiegu@gmail.com>

--- ORG/plugins/employee_management.py
+++ ORG/plugins/employee_management.py
@@ -10,6 +10,14 @@ def register(menu, database):
         table.insert({'name': name})
         print(f"员工 {name} 已添加")
 
+    def list_employees():
+        table = database.get_table('employees')
+        employees = table.all()
+        print("员工列表:")
+        for emp in employees:
+            print(f"- {emp['name']}")
+
     employee_menu.register_option('1', '添加员工', add_employee)
+    employee_menu.register_option('2', '列出员工', list_employees)
 
     menu.register_option('1', '员工管理', submenu=employee_menu)
