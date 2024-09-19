1ac86ccc99d27fcd6df118462992aec64b5a75b4
haojiegu <haojiegu@gmail.com>
2024-09-19 17:21:02 +0800

    优化算法
    Signed-off-by: haojiegu <haojiegu@gmail.com>

--- Jarvis/commands/cmd_jarvis_tools.py
+++ Jarvis/commands/cmd_jarvis_tools.py
@@ -1,3 +1,6 @@
+from sympy.matrices.expressions.kronecker import validate
+
+
 def do_greet(line):
     """Greet the user."""
     print(f"Hello, {"User" if not line else line}!")
@@ -11,3 +14,10 @@ def do_math(line):
     """math <num of questions>"""
     import os
     os.system('python /Users/haojiegu/PycharmProjects/Jasmine/Mathmatics/FFR.py '+line)
+
+def do_scanf(line):
+    from core.base_io import Validate
+    x = Validate()
+    x.input("请输入15-20之间的整数", 'int', (15, 20))
+    x.input("请输入15-20之间的数", 'float', (15, 20))
+    x.input("请输入日期 (格式: YYYY-MM-DD)", 'date')
---
+++ Jarvis/core/base_io.py
@@ -0,0 +1,61 @@
+class Validate:
+    def input(self, prompt, validate, pattern=None):
+        while True:
+            func = getattr(self, validate, self.default)
+            # noinspection PyArgumentList
+            value = func(input(f"{prompt}: "), pattern)
+            if value is not False:
+                return value
+
+    def default(self, info, pattern=None):
+        return info
+
+    def int(self, info, pattern=None):
+        try:
+            int_value = int(info)
+            if pattern is not None:
+                if pattern[0] <= int_value <= pattern[1]:
+                    return int_value
+                else:
+                    DisplayInfo().warning(f"{info} 不在{pattern[0]}-{pattern[1]}之间")
+                    return False
+            else:
+                return int_value
+        except ValueError:
+            DisplayInfo().warning(f"{info} 不是一个整数")
+            return False
+
+    def float(self, info, pattern=None):
+        try:
+            float_value = float(info)
+            if pattern is not None:
+                if pattern[0] <= float_value <= pattern[1]:
+                    return float_value
+                else:
+                    DisplayInfo().warning(f"{info} 不在{pattern[0]}-{pattern[1]}之间")
+                    return False
+            else:
+                return float_value
+        except ValueError:
+            DisplayInfo().warning(f"{info} 不是一个数")
+            return False
+
+    def date(self, info, pattern=None):
+        from datetime import datetime
+        date_format = "%Y-%m-%d"
+        try:
+            date_value = datetime.strptime(info, date_format)
+            return date_value
+        except ValueError:
+            DisplayInfo().warning(f"请输入格式为 {date_format} 的日期")
+            return False
+
+class DisplayInfo:
+    def pprint(self, info):
+        print(f"\n{info}\n{'~'*50}")
+
+    def warning(self, info):
+        self.pprint(f"WARNING: {info}")
+
+    def notice(self, info):
+        self.pprint(f"NOTICE: {info}")
