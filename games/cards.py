b0f23574286bbb52603ffa3928076b5dcdf56946
haojiegu <haojiegu@gmail.com>
2024-10-08 18:31:49 +0800

    优化算法
    Signed-off-by: haojiegu <haojiegu@gmail.com>

--- games/cards.py
+++ games/cards.py
@@ -18,9 +18,31 @@ def random_shuffle_cards(deck_of_cards):
     return res
 
 
-def deal_cards(deck_of_cards, player_num, num_of_cards):
-    pass
+def deal_player_cards(deck_of_cards, player_num, num_of_cards):
+    res = []
+    for _ in range(player_num):
+        res.append([])
+    for _ in range(num_of_cards):
+        for _i in range(player_num):
+            res[_i].append(deck_of_cards.pop())
+    return res
+
+def deal_flop_cards(deck_of_cards):
+    deck_of_cards.pop()
+    return [deck_of_cards.pop(), deck_of_cards.pop(), deck_of_cards.pop()]
+
+def deal_turn_or_river_cards(deck_of_cards):
+    deck_of_cards.pop()
+    return [deck_of_cards.pop()]
 
 
-b = new_deck_of_cards()
-b = random_shuffle(b)
+deck = new_deck_of_cards()
+deck = random_shuffle_cards(deck)
+player_cards = deal_player_cards(deck, 5, 2)
+flop_cards = deal_flop_cards(deck)
+turn_card = deal_turn_or_river_cards(deck)
+river_card = deal_turn_or_river_cards(deck)
+
+print(flop_cards, turn_card, river_card)
+for _ in player_cards:
+    print(_)
